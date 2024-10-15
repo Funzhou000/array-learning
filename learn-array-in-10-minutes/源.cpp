43206711c71a4ba584094fc4dfc65c8be64e4e7f
Funzhou000 <130201517+Funzhou000@users.noreply.github.com>
2024-10-15 19:58:19 +0800

    数组加指针实现的快速打印多个元素
    数组加指针的用法可以快速的打印数组里的多个元素，方法在这里有。这里使用指针p=array（数组的名字等价于它第一个元素的地址）。再用循环指针增加，从而得到各个元素的地址，进行了快速的打印。
    english-description：The use of arrays with pointers can quickly print multiple elements in an array. The method involves using a pointer p = array (the name of the array is equivalent to the address of its first element). By incrementing the pointer within a loop, we obtain the addresses of each element, which enables rapid printing.

---
+++ learn-array-in-10-minutes/源.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int array[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	int* p = array;
+	for (i = 0;i < 10;i++)
+	{
+		printf("%d", *p);
+		p++;
+
+	}
+
+	return 0;
+}
